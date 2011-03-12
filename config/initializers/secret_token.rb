209c87f89b18791afa1bada74b104c49fac232c6
BrotherhoodApps <contactus@brotherhoodapps.co.uk>
2011-03-12 15:58:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShishaApp::Application.config.secret_token = '6a79632d7d47f4b5c1888c1dc0a75f8a09a855a0077dc78cba7ffb345f08dea3957afb1fb7a6a1bd5f39c4e940a1391906c257998986cdc2a54ff4e5d7a259ed'
